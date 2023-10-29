ce077fac89f086de38a73f00e6ee1ea5d72a1bde
gbonavina <132729129+gbonavina@users.noreply.github.com>
2023-10-29 18:50:49 -0300

    Update (1) - randompswrd.c

--- randompswrd.c
+++ randompswrd.c
@@ -9,10 +9,12 @@ int main()
                         '0','1','2','3','4','5','6','7','8','9','!','@','#','$','&','*','(',')','-','_','+','='};
     int c, n;
     int index;
+    int password;
+    FILE *saved_passwords;
 
-    printf("Digite o numero de caracteres da senha (enter the amount of characters): ");
+    printf("Digite o numero de caracteres da senha: ");
     scanf("%d", &c);
-    printf("Digite quantas senhas devem ser geradas (enter the amount of passwords): ");
+    printf("Digite quantas senhas devem ser geradas: ");
     scanf("%d", &n);
 
     char pwrd[n][c + 1];
@@ -28,8 +30,23 @@ int main()
         }
 
         pwrd[i][c] = '\0';
-        printf("Senha %d (Password %d): %s\n", i + 1, i + 1, pwrd[i]);
+        printf("Senha %d: %s\n", i + 1, pwrd[i]);
     }
+
+    saved_passwords = fopen("passwords.txt", "w");
+    if(saved_passwords == NULL) {
+        printf("Erro ao abrir arquivo!");
+        exit(1);
+    }
+    
+    while (password != 0) {
+    printf("Digite qual senha deve ser salva (digite 0 para sair): ");
+    scanf("%i", &password);
+
+    if (password >= 1 && password <= n) {
+        fprintf(saved_passwords, "%s\n", pwrd[password - 1]);
+    }
+}
 
     return 0;
 }
